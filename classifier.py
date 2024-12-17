d6244b732655e9a060a6007e955574c1f9ae4da0
Derrnux <daghzep@gmail.com>
2024-12-17 16:53:09 +0100

    seeding also for cuda

--- classifier.py
+++ classifier.py
@@ -1,7 +1,7 @@
 import torch
 
 class SEQClassifier(torch.nn.Module):
-    def __init__(self, vocab_size, embed_dim, attention_layers, dim_feedforward):
+    def __init__(self, vocab_size, embed_dim, attention_layers, dim_feedforward, seed):
         super(SEQClassifier, self).__init__()
         self.embedding = torch.nn.Embedding(vocab_size, embed_dim)
         self.encoder = torch.nn.TransformerEncoder(
@@ -9,6 +9,13 @@ class SEQClassifier(torch.nn.Module):
             num_layers = attention_layers
         )
         self.fc = torch.nn.Linear(embed_dim, 2)
+
+        torch.manual_seed(seed)
+        if torch.cuda.is_available():
+            torch.cuda.manual_seed(seed)
+            torch.cuda.manual_seed_all(seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
 
     def forward(self, x):
         x = self.embedding(x)
